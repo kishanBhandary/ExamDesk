7bdde6ec645216e1d297e21e6d0616e8f6729aae
kishanBhandary <kishanbhandary0@gmail.com>
2025-12-08 21:41:25 +0530

    add service code of adding students detail

---
+++ src/main/java/com/Examdesk/Examdesk/controller/StudentController.java
@@ -0,0 +1,21 @@
+package com.Examdesk.Examdesk.controller;
+
+import com.Examdesk.Examdesk.model.Student;
+import com.Examdesk.Examdesk.repository.StudentRepository;
+import com.Examdesk.Examdesk.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/students")
+@RestController
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+    @GetMapping("/test")
+    public void addStudent(Student student) {
+        studentService.addStudent(student);
+
+    }
+}
