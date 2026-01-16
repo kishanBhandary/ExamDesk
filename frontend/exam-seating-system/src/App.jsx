650d2f8cf2d8b7d46502d9d063b9489c733d3ffa
kishanBhandary <kishanbhandary0@gmail.com>
2026-01-16 12:49:48 +0530

    add code

--- frontend/exam-seating-system/src/App.jsx
+++ frontend/exam-seating-system/src/App.jsx
@@ -1,34 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app">
+      <img 
+        src="/bg.png" 
+        alt="Background"
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          zIndex: -1,
+          filter: 'blur(4px)'
+        }}
+      />
+      
+      <header 
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '30px 0',
+          zIndex: 5
+        }}
+      >
+        <img 
+          src="/ajiet-logo.svg" 
+          alt="AJIET Logo"
+          style={{
+            width: '650px',
+            height: 'auto'
+          }}
+        />
+      </header>
+    </div>
   )
 }
 
